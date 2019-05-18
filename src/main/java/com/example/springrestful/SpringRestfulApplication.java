2551f6fb98f0e8a84679e77744ea362b7c0e9209
Daniel Carlson <dancarl758@gmail.com>
2019-05-18 09:07:16 +0100

    Create spring boot project

---
+++ src/main/java/com/example/springrestful/SpringRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springrestful/SpringRestfulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springrestful;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestfulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
